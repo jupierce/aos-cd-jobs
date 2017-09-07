555d6a20bfd5a7c56a5b5963c66db86bc66a8c33
Justin Pierce <jupierce@redhat.com>
2017-09-07 16:44:35 -0400

    Update AOS_CD_OPS_DATA.groovy

--- vars/AOS_CD_OPS_DATA.groovy
+++ vars/AOS_CD_OPS_DATA.groovy
@@ -1,13 +1,11 @@
 
 class AOS_CD_OPS_DATA implements Serializable {
-    def getClusterList() {
-        return [ "int:test-key",
+    def clusterList = [ "int:test-key",
                 "int:free-int",
                 "stg:free-stg",
                 "prod:starter-us-east-1",
                 "prod:starter-us-east-2",
                 "prod:starter-us-west-1",
                 "prod:starter-us-west-2",
-        ]
-    }
+    ]
 }
