1f59aa401e9e4b1a9a1a3ef84ad28829e3eb97b3
Justin Pierce <jupierce@redhat.com>
2017-09-07 19:08:39 -0400

    Update and rename acme.groovy to aos_cd_data.groovy

---
+++ vars/aos_cd_data.groovy
@@ -0,0 +1,17 @@
+class aos_cd_data implements Serializable {
+
+    def getClusterList(group) {
+        if ( group == "starter" ) {
+            return [ "int:free-key",
+                    "int:free-int",
+                    "stg:free-stg",
+                    "prod:starter-us-east-1",
+                    "prod:starter-us-east-2",
+                    "prod:starter-us-west-1",
+                    "prod:starter-us-west-2",
+            ]
+        }
+        return [ "int:!UnknkownClusterGroup-" + group ]
+    }
+
+}
