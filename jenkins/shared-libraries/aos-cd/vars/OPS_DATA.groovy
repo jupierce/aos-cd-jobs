15bcf2afa2d3b303b457a6f26b39ef2367abc9bb
Justin Pierce <jupierce@redhat.com>
2017-09-07 16:04:44 -0400

    Create OPS_DATA.groovy

---
+++ jenkins/shared-libraries/aos-cd/vars/OPS_DATA.groovy
@@ -0,0 +1,10 @@
+def getClusterList() {
+    return [ "int:test-key",
+            "int:free-int",
+            "stg:free-stg",
+            "prod:starter-us-east-1",
+            "prod:starter-us-east-2",
+            "prod:starter-us-west-1",
+            "prod:starter-us-west-2",
+    ]
+}
