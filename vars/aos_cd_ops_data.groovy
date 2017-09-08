ececf66255a71ec46a34085f91c3ed5b663912d0
Justin Pierce <jupierce@redhat.com>
2017-09-08 16:04:42 -0400

    Update aos_cd_ops_data.groovy

--- vars/aos_cd_ops_data.groovy
+++ vars/aos_cd_ops_data.groovy
@@ -2,15 +2,22 @@ class aos_cd_ops_data implements Serializable {
 
     def getClusterList(branch) {
         if ( branch.startsWith("cluster") || branch.contains("starter") ) {
-            return [ "int:free-key",
-                    "int:free-int",
-                    "stg:free-stg",
-                    "prod:starter-us-east-1",
-                    "prod:starter-us-east-2",
-                    "prod:starter-us-west-1",
-                    "prod:starter-us-west-2",
+            return [ "starter:int:free-key",
+                    "starter:int:free-int",
+                    "starter:stg:free-stg",
+                    "starter:prod:starter-us-east-1",
+                    "starter:prod:starter-us-east-2",
+                    "starter:prod:starter-us-west-1",
+                    "starter:prod:starter-us-west-2",
             ]
         }
+
+        if ( branch.contains("dedicated") ) {
+            return [ "dedicated:stg:yocum-test-5",
+            ]
+        }
+        
+        
         return [ "int:!UnknkownClusterGroup!-" + branch ]
     }
 
